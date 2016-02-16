ab2a29ce6d4be2597f31175d44c55aff2b869d20
Emma J <codemzy@gmail.com>
2016-02-16 17:55:44 +0000

    Finished app for unauthorised users - working for anon users front and back end

--- public/app/controllers/PollsController.js
+++ public/app/controllers/PollsController.js
@@ -3,5 +3,12 @@ angular.module('VoteGoatApp')
 .controller('PollsController', ['$scope', 'polls', function($scope, polls) {
   polls.success(function(data) {
     $scope.polls = data;
+    $scope.titleLength = function(title) {
+      if (title.length > 40) {
+        return title.slice(0, 37) + "...";
+      } else {
+        return title;
+      }
+    };
   });  
 }]);
