0ef1c52c07148d01042eee3d10eb1d4df402fc7c
Emma J <codemzy@gmail.com>
2016-02-17 16:51:53 +0000

    Updated polls controller and service to call function getPolls()

--- public/app/controllers/PollsController.js
+++ public/app/controllers/PollsController.js
@@ -1,7 +1,7 @@
 // pulls in all poll data (multiple polls)
 angular.module('VoteGoatApp')
 .controller('PollsController', ['$scope', 'polls', function($scope, polls) {
-  polls.success(function(data) {
+  polls.getPolls().success(function(data) {
     $scope.polls = data;
     $scope.titleLength = function(title) {
       if (title.length > 40) {
--- public/app/services/polls.js
+++ public/app/services/polls.js
@@ -1,10 +1,13 @@
 angular.module('VoteGoatApp')
 .factory('polls', ['$http', function($http) {
-  return $http.get('/api/polls')
-            .success(function(data) {
-              return data;
-            })
-            .error(function(err) {
-              return err;
-            });
+  this.getPolls = function() {
+    return $http.get('/api/polls')
+              .success(function(data) {
+                return data;
+              })
+              .error(function(err) {
+                return err;
+              });
+  };
+  return this;
 }]);
